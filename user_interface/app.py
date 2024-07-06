1b91b4efa9befadc7e47116923cefe558bdc2da1
Dustin Maselbas <dmaselbas@gmail.com>
2024-07-05 23:34:27 -0600

    gps works

--- user_interface/app.py
+++ user_interface/app.py
@@ -1,38 +1,47 @@
 # user_interface/app.py
 
-import dash
-from dash import dcc, html
-from dash.dependencies import Input, Output
+import streamlit as st
+from servicess.navigation_service import NavigationService
 
-# Initialize the Dash app
-app = dash.Dash(__name__)
+# Initialize the NavigationService instance
+navigation_service = NavigationService()
 
-# Define the layout of the app
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+# Create a new Streamlit tab
+st.tab("Navigation Controller")
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
+# Create a layout with buttons for each directional method
+col1, col2, col3, col4 = st.columns(4)
 
-    dcc.Input(id='input-box', type='text'),
-    html.Button('Submit', id='button'),
-    html.Div(id='output-container-button',
-             children='Enter a value and press submit')
-])
+# Add a video component to the left of the direction buttons
+with col1:
+    st.video(src="http://<raspberry_pi_ip>:8000/video.mjpg", format="video/mjpg")
 
-# Define callback to update the output based on input
-@app.callback(
-    Output('output-container-button', 'children'),
-    Input('button', 'n_clicks'),
-    Input('input-box', 'value')
-)
-def update_output(n_clicks, value):
-    if n_clicks is None:
-        return 'Enter a value and press submit'
-    else:
-        return f'The input value is: {value}'
+with col2:
+    if st.button("Left"):
+        navigation_service.move_left()
+
+with col3:
+    if st.button("Up"):
+        navigation_service.set_speed(50)
+
+with col4:
+    if st.button("Right"):
+        navigation_service.move_right()
+
+with st.expander("Advanced Controls"):
+    col5, col6 = st.columns(2)
+
+    with col5:
+        speed = st.slider("Speed", min_value=0, max_value=100, value=0)
+        if st.button("Set Speed"):
+            navigation_service.set_speed(speed)
+
+    with col6:
+        if st.button("Stop"):
+            navigation_service.stop()
 
 # Run the app
-if __name__ == '__main__':
-    app.run_server(debug=True)
+if __name__ == "__main__":
+    st.title("Weed Fucker 5000")
+    st.sidebar.title("Navigation")
+    st.sidebar.subheader("Control the robot's movement")
